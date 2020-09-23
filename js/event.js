0494bddc31086388d0ed80b78bc3d94f8ecc066f
ElmerAntonio <war_listen34@users.noreply.github.com>
2020-09-23 02:05:13 -0500

    V
    V2.1

---
+++ js/event.js
@@ -0,0 +1,11 @@
+function message() {
+  var intro = document.getElementById('intro');
+  intro.style.backgroundColor = '#a6a6e0';
+}
+
+function cambio() {
+  var cam = document.getElementById('intro');
+  intro.style.backgroundColor = '#ffffff80';
+}
+
+function nomb()
